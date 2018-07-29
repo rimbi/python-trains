ee892932ea717aebab794b5d4eb26a80ab373d23
Cem Eliguzel <cemeliguzel@gmail.com>
2018-07-29 12:12:40 +0200

    Implements direct route calculation when multiple direct routes provided

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 class Setup(object):
     def __init__(self, config):
         self.config = config
-    
+        self.routes = {'{}-{}'.format(r[0], r[1]): int(r[2:])
+                       for r in config.split('-')}
+
     def get_distance(self, route):
-        return int(self.config[2:])
+        return self.routes[route]
+
 
 if __name__ == '__main__':
     print 'Hello, world!'
--- test_main.py
+++ test_main.py
@@ -8,3 +8,11 @@ def test_setup_should_work_with_single_route_info():
     distance = setup.get_distance('A-B')
     # then
     expect(distance).to(be(5))
+
+def test_setup_should_work_with_multiple_route_info():
+    # given
+    setup = Setup('AB5-CD4-DE6')
+    # when
+    distance = setup.get_distance('C-D')
+    # then
+    expect(distance).to(be(4))
